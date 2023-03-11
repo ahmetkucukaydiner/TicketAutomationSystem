c66fd87e12682f58ee0783b0c1b9e6c25167ac3c
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2023-03-11 23:33:54 +0300

    Bus & busowners controllers added

---
+++ WebAPI/Controllers/BusOwnersController.cs
@@ -0,0 +1,83 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusOwnersController : ControllerBase
+    {
+        private IBusOwnerService _busOwnerService;
+
+        public BusOwnersController(IBusOwnerService busOwnerService)
+        {
+            _busOwnerService = busOwnerService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _busOwnerService.GetAll();
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _busOwnerService.GetById(id);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(BusOwner busOwner)
+        {
+            var result = _busOwnerService.Add(busOwner);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(BusOwner busOwner)
+        {
+            var result = _busOwnerService.Add(busOwner);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(BusOwner busOwner)
+        {
+            var result = _busOwnerService.Update(busOwner);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Message);
+        }
+    }
+}
---
+++ WebAPI/Controllers/BusesController.cs
@@ -0,0 +1,83 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusesController : ControllerBase
+    {
+        private IBusService _busService;
+
+        public BusesController(IBusService busService)
+        {
+            _busService = busService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _busService.GetAll();
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Success);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _busService.GetById(id);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Bus bus)
+        {
+            var result = _busService.Add(bus);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(Bus bus)
+        {
+            var result = _busService.Delete(bus);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Bus bus)
+        {
+            var result = _busService.Update(bus);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Message);
+        }
+    }
+}
